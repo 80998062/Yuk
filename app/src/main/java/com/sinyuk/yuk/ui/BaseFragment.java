6000cb429ee7e4b268d6439cc28674b609a98004
沈烨坷 <80998062@qq.com>
2016-08-10 00:50:01 +0800

    Refactor on base fragment

--- app/src/main/java/com/sinyuk/yuk/ui/BaseFragment.java
+++ app/src/main/java/com/sinyuk/yuk/ui/BaseFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,10 +21,11 @@ public abstract class BaseFragment extends Fragment {
     private Unbinder unbinder;
     private CompositeSubscription mCompositeSubscription;
     protected Context mContext = null;
-
+    private static final String STATE_SAVE_IS_HIDDEN = "STATE_SAVE_IS_HIDDEN";
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+
         mContext = context;
     }
 
@@ -36,8 +38,29 @@ public abstract class BaseFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            boolean isSupportHidden = savedInstanceState.getBoolean(STATE_SAVE_IS_HIDDEN);
+
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            if (isSupportHidden) {
+                ft.hide(this);
+            } else {
+                ft.show(this);
+            }
+            ft.commit();
+        }
         mCompositeSubscription = new CompositeSubscription();
         beforeInflate();
+    }
+
+    /**
+     * 保存Fragment的Hidden状态
+     * 避免内存重启导致的Fragment重叠
+     * @param outState
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(STATE_SAVE_IS_HIDDEN, isHidden());
     }
 
     protected abstract void beforeInflate();
