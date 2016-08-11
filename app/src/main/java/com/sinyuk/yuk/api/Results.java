b6b32d77a671d04ddc09351bc2648a559206509a
沈烨坷 <80998062@qq.com>
2016-08-11 16:02:14 +0800

    Add Results call to check the request

--- app/src/main/java/com/sinyuk/yuk/api/Results.java
+++ app/src/main/java/com/sinyuk/yuk/api/Results.java
@@ -3,6 +3,9 @@ package com.sinyuk.yuk.api;
 import retrofit2.adapter.rxjava.Result;
 import rx.functions.Func1;
 
+/**
+ * 网络请求的结果
+ */
 public final class Results {
   private static final Func1<Result<?>, Boolean> SUCCESSFUL =
           result -> !result.isError() && result.response().isSuccessful();
