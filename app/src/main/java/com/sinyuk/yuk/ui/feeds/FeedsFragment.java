daf9966e07d01725f9cd6a41179f3245f76403fa
沈烨坷 <80998062@qq.com>
2016-08-13 11:37:50 +0800

    Inject views

--- app/src/main/java/com/sinyuk/yuk/ui/feeds/FeedsFragment.java
+++ app/src/main/java/com/sinyuk/yuk/ui/feeds/FeedsFragment.java
@@ -1,8 +1,13 @@
 package com.sinyuk.yuk.ui.feeds;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
 import com.f2prateek.rx.preferences.RxSharedPreferences;
@@ -13,8 +18,9 @@ import com.sinyuk.yuk.data.shot.DaggerShotRepositoryComponent;
 import com.sinyuk.yuk.data.shot.Shot;
 import com.sinyuk.yuk.data.shot.ShotRepository;
 import com.sinyuk.yuk.ui.BaseFragment;
-import com.sinyuk.yuk.utils.lists.ListItemMarginDecoration;
+import com.sinyuk.yuk.utils.BetterViewAnimator;
 import com.sinyuk.yuk.utils.PrefsUtils;
+import com.sinyuk.yuk.utils.lists.ListItemMarginDecoration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +28,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import fr.castorflex.android.smoothprogressbar.SmoothProgressBar;
 import rx.functions.Action1;
 import timber.log.Timber;
@@ -30,14 +37,30 @@ import timber.log.Timber;
  * Created by Sinyuk on 16/7/1.
  */
 public class FeedsFragment extends BaseFragment {
-    @BindView(R.id.recycler_view)
-    RecyclerView mRecyclerView;
-    @BindView(R.id.progress_bar)
-    SmoothProgressBar mSmoothProgressBar;
     @Inject
     ShotRepository shotRepository;
     @Inject
     RxSharedPreferences mSharedPreferences;
+
+    @BindView(R.id.loading_layout)
+    RelativeLayout mLoadingLayout;
+
+    @BindView(R.id.layout_list)
+    RelativeLayout mListLayout;
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.progress_bar)
+    SmoothProgressBar mSmoothProgressBar;
+
+    @BindView(R.id.layout_error)
+    RelativeLayout mLayoutError;
+
+    @BindView(R.id.layout_empty)
+    RelativeLayout mLayoutEmpty;
+
+    @BindView(R.id.view_animator)
+    BetterViewAnimator mViewAnimator;
+
     private FeedsAdapter mAdapter;
     private ArrayList<Shot> mShotList = new ArrayList<>();
     private int mPage;
@@ -69,7 +92,6 @@ public class FeedsFragment extends BaseFragment {
     protected void finishInflate() {
         initRecyclerView();
         mRecyclerView.postDelayed(() -> loadFeeds(DribbleApi.ANIMATED, 1), 0);
-        mSmoothProgressBar.progressiveStart();
     }
 
     private void initRecyclerView() {
@@ -90,6 +112,16 @@ public class FeedsFragment extends BaseFragment {
                     mAdapter.setAutoPlayGif(autoPlayGif);
                 });
 
+        mAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onChanged() {
+                mViewAnimator.setDisplayedChildId(mAdapter.getItemCount() == 0
+                        ? R.id.layout_empty //
+                        : R.id.layout_list);
+                /*swipeRefreshView.setRefreshing(false);*/
+            }
+        });
+
     }
 
     //    @Subscribe(threadMode = ThreadMode.MAIN)
@@ -109,4 +141,5 @@ public class FeedsFragment extends BaseFragment {
                     }
                 });
     }
+    
 }
