50b38f57353d625c8cf5e5377c3eca0e7291f836
沈烨坷 <80998062@qq.com>
2016-08-16 00:21:16 +0800

    Implement the base recycler view adapter

--- app/src/main/java/com/sinyuk/yuk/ui/feeds/FeedsAdapter.java
+++ app/src/main/java/com/sinyuk/yuk/ui/feeds/FeedsAdapter.java
@@ -6,10 +6,10 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import com.bumptech.glide.DrawableRequestBuilder;
-import com.bumptech.glide.Priority;
 import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.sinyuk.yuk.R;
+import com.sinyuk.yuk.data.BaseRVAdapter;
 import com.sinyuk.yuk.data.shot.Shot;
 import com.sinyuk.yuk.utils.glide.CropCircleTransformation;
 
@@ -23,57 +23,38 @@ import timber.log.Timber;
  * Created by Sinyuk on 16/7/6.
  * 尽量减少这里的逻辑
  */
-public class FeedsAdapter extends RecyclerView.Adapter<FeedsAdapter.FeedItemViewHolder> implements Action1<List<Shot>> {
+public class FeedsAdapter extends BaseRVAdapter<Shot> implements Action1<List<Shot>> {
     private final DrawableRequestBuilder<String> avatarBuilder;
     private final DrawableRequestBuilder<String> GIFBuilder;
     private final DrawableRequestBuilder<String> PNGBuilder;
-
-    private List<Shot> mDataSet = new ArrayList<>();
     private boolean isAutoPlayGif = false;
 
     public FeedsAdapter(Context context, RequestManager requestManager, ArrayList<Shot> dataSet) {
+        super(dataSet);
         Timber.tag("FeedsAdapter");
-        this.GIFBuilder = requestManager
-                .fromString()
-                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                .dontAnimate()
-                .centerCrop();
-
-        this.PNGBuilder = requestManager
-                .fromString()
-                .diskCacheStrategy(DiskCacheStrategy.RESULT)
-                .dontAnimate()
-                .centerCrop();
-
-        this.mDataSet = dataSet;
-
-        avatarBuilder = requestManager.fromString()
-                .priority(Priority.NORMAL)
-                .diskCacheStrategy(DiskCacheStrategy.RESULT)
-                .centerCrop()
-                .dontAnimate()
-                .bitmapTransform(new CropCircleTransformation(context));
+        GIFBuilder = requestManager.fromString().diskCacheStrategy(DiskCacheStrategy.SOURCE).centerCrop();
+        PNGBuilder = requestManager.fromString().diskCacheStrategy(DiskCacheStrategy.RESULT).centerCrop();
+        avatarBuilder = requestManager.fromString().diskCacheStrategy(DiskCacheStrategy.RESULT).centerCrop().bitmapTransform(new CropCircleTransformation(context));
     }
 
+    @Override
+    protected void footerOnVisibleItem() {
+
+    }
 
     @Override
-    public FeedItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateMyItemViewHolder(ViewGroup parent, int viewType) {
         return new FeedItemViewHolder((FeedItemView) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.feed_list_item, parent, false));
     }
 
     @Override
-    public int getItemCount() {
-        return mDataSet.size();
-    }
-
-    @Override
-    public void onBindViewHolder(FeedItemViewHolder holder, int position) {
-        final Shot data = mDataSet.get(position);
+    public void onBindMyItemViewHolder(RecyclerView.ViewHolder holder, int position) {
+        final Shot data = getAll().get(position);
         if (data.isAnimated()) {
-            holder.bindTo(data, GIFBuilder, isAutoPlayGif, avatarBuilder);
+            ((FeedItemViewHolder) holder).bindTo(data, GIFBuilder, isAutoPlayGif, avatarBuilder);
         } else {
-            holder.bindTo(data, PNGBuilder, isAutoPlayGif, avatarBuilder);
+            ((FeedItemViewHolder) holder).bindTo(data, PNGBuilder, isAutoPlayGif, avatarBuilder);
         }
     }
 
@@ -84,25 +65,24 @@ public class FeedsAdapter extends RecyclerView.Adapter<FeedsAdapter.FeedItemView
 
     @Override
     public void call(List<Shot> shots) {
-        mDataSet = shots;
-        notifyDataSetChanged();
+        reset(shots);
         Timber.d("Data in adapter %s", shots.toString());
     }
 
 
     public class FeedItemViewHolder extends RecyclerView.ViewHolder {
-        private final FeedItemView itemView;
+        private final FeedItemView feedItemView;
 
-        public FeedItemViewHolder(FeedItemView itemView) {
-            super(itemView);
-            this.itemView = itemView;
+        public FeedItemViewHolder(FeedItemView feedItemView) {
+            super(feedItemView);
+            this.feedItemView = feedItemView;
         }
 
         public void bindTo(Shot data,
                            DrawableRequestBuilder<String> shotBuilder,
                            boolean isAutoPlayGif,
                            DrawableRequestBuilder<String> avatarBuilder) {
-            itemView.bindTo(data, shotBuilder, isAutoPlayGif, avatarBuilder);
+            feedItemView.bindTo(data, shotBuilder, isAutoPlayGif, avatarBuilder);
         }
 
     }
