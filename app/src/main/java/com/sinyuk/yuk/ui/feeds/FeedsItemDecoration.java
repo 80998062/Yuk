cd1f506b1361da5b39dc49cb40191bd9e9943c14
沈烨坷 <80998062@qq.com>
2016-08-17 18:31:37 +0800

    Create feed item decorator

---
+++ app/src/main/java/com/sinyuk/yuk/ui/feeds/FeedsItemDecoration.java
@@ -0,0 +1,34 @@
+package com.sinyuk.yuk.ui.feeds;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.sinyuk.yuk.utils.ScreenUtils;
+
+/**
+ * Created by Sinyuk on 16/8/17.
+ */
+public class FeedsItemDecoration extends RecyclerView.ItemDecoration {
+
+
+    private final int marginTop;
+    private final int gap;
+
+    public FeedsItemDecoration(Context context) {
+        marginTop = ScreenUtils.dpToPxInt(context, 8);
+        gap = ScreenUtils.dpToPxInt(context, 12);
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        int position = parent.getChildAdapterPosition(view); // item position
+        if (position == 0) {
+            outRect.top = marginTop;
+        } else {
+            outRect.top = gap;
+
+        }
+    }
+}
