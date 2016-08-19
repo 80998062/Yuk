95b9bc7b022df984736719e16542b2291aaa0a5b
沈烨坷 <80998062@qq.com>
2016-08-19 15:42:06 +0800

    Add splash activity to manifest

--- app/src/main/java/com/sinyuk/yuk/ui/splash/SplashActivity.java
+++ app/src/main/java/com/sinyuk/yuk/ui/splash/SplashActivity.java
@@ -1,8 +1,25 @@
 package com.sinyuk.yuk.ui.splash;
 
+import com.sinyuk.yuk.App;
+import com.sinyuk.yuk.ui.BaseActivity;
+
 /**
  * Created by Sinyuk on 16/8/16.
  */
-public class SplashActivity {
+public class SplashActivity extends BaseActivity{
 
+    @Override
+    protected int getContentViewID() {
+        return 0;
+    }
+
+    @Override
+    protected void beforeInflating() {
+        App.get(this).getAppComponent().plus(new SplashModule(this)).inject(this);
+    }
+
+    @Override
+    protected void finishInflating() {
+
+    }
 }
