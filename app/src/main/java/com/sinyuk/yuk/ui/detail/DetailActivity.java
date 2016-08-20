0aebcf8f52bd170fbb988b6ae1f2cbd665208543
沈烨坷 <80998062@qq.com>
2016-08-20 13:19:30 +0800

    Create activity for display shot details

---
+++ app/src/main/java/com/sinyuk/yuk/ui/detail/DetailActivity.java
@@ -0,0 +1,25 @@
+package com.sinyuk.yuk.ui.detail;
+
+import com.sinyuk.yuk.R;
+import com.sinyuk.yuk.ui.BaseActivity;
+
+/**
+ * Created by Sinyuk on 16/8/20.
+ */
+public class DetailActivity extends BaseActivity {
+
+    @Override
+    protected int getContentViewID() {
+        return R.layout.detail_activity;
+    }
+
+    @Override
+    protected void beforeInflating() {
+
+    }
+
+    @Override
+    protected void finishInflating() {
+
+    }
+}
