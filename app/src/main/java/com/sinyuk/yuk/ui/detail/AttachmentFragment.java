32340e27de2902469bcc1266525ce77a0acc7003
沈烨坷 <80998062@qq.com>
2016-09-09 18:59:43 +0800

    Refactor on attachment fragment

--- app/src/main/java/com/sinyuk/yuk/ui/detail/AttachmentFragment.java
+++ app/src/main/java/com/sinyuk/yuk/ui/detail/AttachmentFragment.java
@@ -2,6 +2,7 @@ package com.sinyuk.yuk.ui.detail;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SnapHelper;
@@ -10,23 +11,28 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.sinyuk.yuk.App;
 import com.sinyuk.yuk.R;
 import com.sinyuk.yuk.api.DribbleService;
 import com.sinyuk.yuk.data.shot.Attachment;
 import com.sinyuk.yuk.ui.BaseFragment;
 import com.sinyuk.yuk.utils.lists.GravitySnapHelper;
-import com.sinyuk.yuk.widgets.FontTextView;
+import com.sinyuk.yuk.utils.lists.SlideInUpAnimator;
+import com.sinyuk.yuk.widgets.FourThreeImageView;
 
+import java.util.Collections;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
-import rx.Observer;
+import butterknife.ButterKnife;
+import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
 /**
@@ -34,7 +40,6 @@ import rx.schedulers.Schedulers;
  */
 public class AttachmentFragment extends BaseFragment {
     private static final String KEY_ID = "ID";
-    private static final String KEY_COUNT = "COUNT";
     private static final String TAG = "AttachmentFragment";
 
     @BindView(R.id.recycler_view)
@@ -43,12 +48,12 @@ public class AttachmentFragment extends BaseFragment {
     @Inject
     DribbleService dribbleService;
     private long mId;
-    private int mCount;
+    private AttachmentAdapter mAdapter;
+    private List<Attachment> mAttachmentList = Collections.emptyList();
 
-    public static AttachmentFragment newInstance(long id, int attachmentCount) {
+    public static AttachmentFragment newInstance(long id) {
         Bundle args = new Bundle();
         args.putLong(KEY_ID, id);
-        args.putInt(KEY_COUNT, attachmentCount);
 
         AttachmentFragment fragment = new AttachmentFragment();
         fragment.setArguments(args);
@@ -58,7 +63,6 @@ public class AttachmentFragment extends BaseFragment {
     @Override
     protected void beforeInflate() {
         mId = getArguments().getLong(KEY_ID);
-        mCount = getArguments().getInt(KEY_COUNT);
     }
 
     @Override
@@ -76,31 +80,23 @@ public class AttachmentFragment extends BaseFragment {
     protected void finishInflate() {
         initRecyclerView();
         createPlaceHolder();
+        initAdapter();
         loadAttachments();
+    }
+
+    private void initAdapter() {
+        mAdapter = new AttachmentAdapter();
+
+        mAdapter.setHasStableIds(true);
+
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     private void loadAttachments() {
         dribbleService.attachments(mId).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<List<Attachment>>() {
-                    @Override
-                    public void onCompleted() {
-                        Log.d(TAG, "onCompleted: ");
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.e(TAG, "onError: " + e.getLocalizedMessage());
-                    }
-
-                    @Override
-                    public void onNext(List<Attachment> attachments) {
-                        for (int i = 0; i < attachments.size(); i++) {
-                            Log.d(TAG, "onNext:");
-                            Log.d(TAG, attachments.get(i).toString());
-                        }
-
-                    }
-                });
+                .flatMap(Observable::from)
+                .doOnError(Throwable::printStackTrace)
+                .subscribe(mAdapter);
     }
 
     private void initRecyclerView() {
@@ -116,6 +112,8 @@ public class AttachmentFragment extends BaseFragment {
 
         mRecyclerView.setScrollingTouchSlop(RecyclerView.TOUCH_SLOP_PAGING);
 
+        mRecyclerView.setItemAnimator(new SlideInUpAnimator(new FastOutSlowInInterpolator()));
+
 //        mRecyclerView.addItemDecoration(new FeedsItemDecoration(getContext()));
     }
 
@@ -123,28 +121,51 @@ public class AttachmentFragment extends BaseFragment {
 
     }
 
-    private class AttachmentAdapter extends RecyclerView.Adapter<AttachmentAdapter.AttachmentViewHolder> {
+    private class AttachmentAdapter extends RecyclerView.Adapter<AttachmentAdapter.AttachmentViewHolder>
+            implements Action1<Attachment> {
+
 
         @Override
-        public AttachmentAdapter.AttachmentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public AttachmentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             return new AttachmentViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.detail_activity_attachment_item, parent, false));
         }
 
         @Override
         public void onBindViewHolder(AttachmentViewHolder holder, int position) {
+            if (mAttachmentList.get(position) != null) {
+                final Attachment data = mAttachmentList.get(position);
+                Glide.with(AttachmentFragment.this).load(data.getThumbnailUrl())
+                        .error(R.drawable.pic_fill)
+                        .placeholder(R.drawable.pic_fill)
+                        .crossFade(400)
+                        .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                        .into(holder.mThumbnail);
 
+                holder.mThumbnail.setOnClickListener(view -> {
+                    Log.d(TAG, "Click At " + position);
+                });
+            }
         }
 
 
         @Override
         public int getItemCount() {
-            return mCount;
+            return mAttachmentList.size();
+        }
+
+        @Override
+        public void call(Attachment attachment) {
+            mAttachmentList.add(attachment);
+            notifyItemInserted(mAttachmentList.size() - 1);
         }
 
         public class AttachmentViewHolder extends RecyclerView.ViewHolder {
+            @BindView(R.id.thumbnail)
+            FourThreeImageView mThumbnail;
 
             public AttachmentViewHolder(View itemView) {
                 super(itemView);
+                ButterKnife.bind(this, itemView);
             }
         }
     }
